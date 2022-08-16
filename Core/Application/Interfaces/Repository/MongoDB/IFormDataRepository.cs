b19ce43b672204cd7590924bd59a95f1eeaa7d76
Olaniyan Elias <elias.akin@gmail.com>
2022-08-16 19:27:39 +0100

    completed concrete formdatarepo

--- Core/Application/Interfaces/Repository/MongoDB/IFormDataRepository.cs
+++ Core/Application/Interfaces/Repository/MongoDB/IFormDataRepository.cs
@@ -10,6 +10,6 @@ namespace Core.Application.Interfaces.Repository.MongoDB {
         Task<List<FormData>> getFormData(string schemaID, List<KeyValuePair<string, dynamic>> field);
         Task<bool> updateFormData(FormData data);
         Task<bool> createFormData(FormData data);
-        Task<bool> deleteFormData(FormData data);
+        Task<bool> deleteFormData(string id);
     }
 }
--- Infrastructure/Repository/MongoDB/FormDataRepository.cs
+++ Infrastructure/Repository/MongoDB/FormDataRepository.cs
@@ -4,6 +4,7 @@ using Infrastructure.Persistence;
 using NetCore.AutoRegisterDi;
 using System;
 using System.Collections.Generic;
+using System.Linq.Expressions;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -11,24 +12,31 @@ namespace Infrastructure.Repository.MongoDB {
     [RegisterAsScoped]
     public class FormDataRepository : Repository<FormData>, IFormDataRepository, Interfaces.MongoDB.IRepository<FormData> {
         public FormDataRepository(IMongoDb db) : base(db) { }
-        public Task<bool> createFormData(FormData data) {
-            throw new NotImplementedException();
+        public async Task<bool> createFormData(FormData data) {
+            return await create(data);
         }
 
-        public Task<bool> deleteFormData(FormData data) {
-            throw new NotImplementedException();
+        public async Task<bool> deleteFormData(string id) {
+            Expression<Func<FormData, bool>> condition = F => F.id == id;
+            return await delete(condition);
         }
 
-        public Task<List<FormData>> getFormData(string schemaID) {
-            throw new NotImplementedException();
+        public async Task<List<FormData>> getFormData(string schemaID) {
+            Expression<Func<FormData, bool>> condition = F => F.schemaID == schemaID;
+            return (List<FormData>)await getByCondition(condition);
         }
 
-        public Task<List<FormData>> getFormData(string schemaID, List<KeyValuePair<string, dynamic>> field) {
-            throw new NotImplementedException();
+        public async Task<List<FormData>> getFormData(string schemaID, List<KeyValuePair<string, dynamic>> field) {
+            var data = await this.getFormData(schemaID);
+            foreach(KeyValuePair<string, dynamic> search in field) {
+                data = data.FindAll(G => G.data[search.Key] == search.Value);
+            }
+            return data;
         }
 
-        public Task<bool> updateFormData(FormData data) {
-            throw new NotImplementedException();
+        public async Task<bool> updateFormData(FormData data) {
+            Expression<Func<FormData, bool>> condition = F => F.id == data.id;
+            return await update(new { data = data.data }, condition);
         }
     }
 }
