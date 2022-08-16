aed9ed15ebbea3d4c7077ddae9576f5df81dca70
Olaniyan Elias <elias.akin@gmail.com>
2022-08-16 19:14:04 +0100

    valueobjs

---
+++ Core/Models/ValueObjects/SchemaField.cs
@@ -0,0 +1,16 @@
+﻿using Core.Models.Enums;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Core.Models.ValueObjects {
+    public class SchemaField {
+        public string fieldName { get; set; }
+        public string fieldID { get; set; }
+        public FieldType fieldType { get; set; }
+        public List<FieldBehaviour> behavior { get; set; }
+        public bool hide { get; set; }
+        public string[] data { get; set; }
+        public string dataSource { get; set; }
+    }
+}
