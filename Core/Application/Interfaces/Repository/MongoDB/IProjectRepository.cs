0ce4354c54bb5bc8e1fda3846ab7165ab3b54d93
Olaniyan Elias <elias.akin@gmail.com>
2022-08-16 19:13:27 +0100

    project repo

---
+++ Core/Application/Interfaces/Repository/MongoDB/IProjectRepository.cs
@@ -0,0 +1,14 @@
+﻿using Core.Models.Entities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.Application.Interfaces.Repository.MongoDB {
+    public interface IProjectRepository {
+        Task<bool> createProject(Project project);
+        Task<List<Project>> getProject();
+        Task<List<Project>> getProject(string id);
+        Task<bool> endProject(string id);
+    }
+}
