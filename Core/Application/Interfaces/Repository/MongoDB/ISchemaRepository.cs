a0753b7a4ee1420fbc85c0dd6d188f77b97c599e
Olaniyan Elias <elias.akin@gmail.com>
2022-08-16 19:13:35 +0100

    schema repo

---
+++ Core/Application/Interfaces/Repository/MongoDB/ISchemaRepository.cs
@@ -0,0 +1,16 @@
+﻿using Core.Models.Entities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.Application.Interfaces.Repository.MongoDB {
+    public interface ISchemaRepository {
+        Task<bool> createSchema(Schema schema);
+        Task<List<Schema>> getSchema();
+        Task<List<Schema>> getSchemaById(string schemaID);
+        Task<List<Schema>> getSchemaByProject(string projectID);
+        Task<bool> updateSchema(Schema schema);
+        Task<bool> lockSchema(string id);
+    }
+}
