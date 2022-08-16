0619ab84b54227a5811de16ec4999a54a3182af7
Olaniyan Elias <elias.akin@gmail.com>
2022-08-16 19:12:20 +0100

    dtos

---
+++ Core/Application/DTOs/Request/ProjectDTO.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Core.Application.DTOs.Request {
+    public class ProjectDTO {
+        public string id { get; set; }
+        public string projectName { get; set; }
+        public string projectDetails { get; set; }
+        public long dateCreated { get; set; }
+        public int status { get; set; }
+    }
+}
---
+++ Core/Application/DTOs/Request/SchemaDTO.cs
@@ -0,0 +1,15 @@
+﻿using Core.Models.ValueObjects;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Core.Application.DTOs.Request {
+    public class SchemaDTO {
+        public string id { get; set; }
+        public string projectID { get; set; }
+        public string schemaName { get; set; }
+        public string schemaID { get; set; }
+        public bool locked { get; set; } = false;
+        public List<SchemaField> SchemaField { get; set; }
+    }
+}
