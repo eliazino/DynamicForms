714d8413ba390c9e1c67f07c681fbadaa741e575
Olaniyan Elias <elias.akin@gmail.com>
2022-08-16 19:13:17 +0100

    formdata interface

---
+++ Core/Application/Interfaces/Repository/MongoDB/IFormDataRepository.cs
@@ -0,0 +1,15 @@
+﻿using Core.Models.Entities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.Application.Interfaces.Repository.MongoDB {
+    public interface IFormDataRepository {
+        Task<List<FormData>> getFormData(string schemaID);
+        Task<List<FormData>> getFormData(string schemaID, List<KeyValuePair<string, dynamic>> field);
+        Task<bool> updateFormData(FormData data);
+        Task<bool> createFormData(FormData data);
+        Task<bool> deleteFormData(FormData data);
+    }
+}
