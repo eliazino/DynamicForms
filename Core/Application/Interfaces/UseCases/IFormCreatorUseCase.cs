b15d207ad0f93d936d3622a4a8fb4c5f88fa5478
Olaniyan Elias <elias.akin@gmail.com>
2022-08-17 15:38:46 +0100

    usecase interface

---
+++ Core/Application/Interfaces/UseCases/IFormCreatorUseCase.cs
@@ -0,0 +1,22 @@
+﻿using Core.Application.DTOs.Request;
+using Core.Application.DTOs.Response;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using static Core.Application.DTOs.Response.ResponseFormat;
+
+namespace Core.Application.Interfaces.UseCases {
+    public interface IFormCreatorUseCase {
+        Task<RawResponse> createProject(ProjectDTO dto);
+        Task<RawResponse> closeProject(ProjectDTO dto);
+        Task<RawResponse> getProject();
+        Task<RawResponse> createSchema(SchemaDTO dto);        
+        Task<RawResponse> lockSchema(SchemaDTO dto);
+        Task<RawResponse> getSchemaByID(string schemaID);
+        Task<RawResponse> getSchemaByProject(string projectID);
+        Task<RawResponse> addData(FormDataDTO dto);
+        Task<RawResponse> getData(DataFilterDTO filter);
+        Task<RawResponse> deleteData(string id);
+    }
+}
