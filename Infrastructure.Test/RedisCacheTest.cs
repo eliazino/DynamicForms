18208884417e81de74158d8150b03f829b558b1d
Olaniyan Elias <elias.akin@gmail.com>
2022-08-18 23:31:19 +0100

    redis cache test

---
+++ Infrastructure.Test/RedisCacheTest.cs
@@ -0,0 +1,19 @@
+﻿using Core.Application.Interfaces.Repository.Cache;
+using Infrastructure.Interfaces;
+using Infrastructure.Persistence;
+using NUnit.Framework;
+using StackExchange.Redis.Extensions.Core.Abstractions;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Infrastructure.Test {
+    [TestFixture]
+    public class RedisCacheTest {
+        [TestCase]
+        public void TestAddString() {
+            //RedisCacheClient cl = new RedisCacheClient();
+            //ICacheService _request = (ICacheService)new RedisCache();            
+        }
+    }
+}
