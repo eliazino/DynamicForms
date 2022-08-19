40d305c7b8d3988ee5582cde6bb21bbc1b9e0b4a
Olaniyan Elias <elias.akin@gmail.com>
2022-08-19 10:25:53 +0100

    tests

---
+++ Core.Test/ProjectTest.cs
@@ -0,0 +1,36 @@
+﻿using Core.Exceptions;
+using Core.Models.Entities;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Core.Test {
+    [TestFixture]
+    public class ProjectTest {
+
+        [TestCase]
+        public void TestConstructor() {
+            var dto = new Application.DTOs.Request.ProjectDTO {
+                projectName = "Sample",
+                projectDetails = "Details"
+            };
+            Project project = new Project(
+                dto
+                );
+            Assert.IsTrue(project.projectName == dto.projectName, "Project Name should be set");
+            Assert.IsTrue(project.projectDetails == dto.projectDetails, "Project Details should be set");
+            Assert.IsTrue(project.status == 1, "Default status is 1");
+        }
+
+        [TestCase]
+        public void TestConstructorThrowsError() {
+            var dto = new Application.DTOs.Request.ProjectDTO {
+                projectName = "Sample"
+            };
+            Assert.Throws<InputError>(()=> {
+                new Project(dto);
+            });
+        }
+    }
+}
