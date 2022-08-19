a3329b0caeb1b8e7613e945c06bd905cf9778783
Olaniyan Elias <elias.akin@gmail.com>
2022-08-19 10:26:01 +0100

    bug fix

--- Core/Models/Entities/BaseEntity.cs
+++ Core/Models/Entities/BaseEntity.cs
@@ -6,11 +6,11 @@ using System.Text;
 namespace Core.Models.Entities {
     public class BaseEntity {
         public bool isNullOrEmpty(params string[] str) {
-            return str.All(s => string.IsNullOrEmpty(s));
+            return !str.All(s => !string.IsNullOrEmpty(s));
         }
 
         public bool isNull(params object[] objs) {
-            return objs.All(s => s == null);
+            return !objs.All(s => s != null);
         }
     }
 }
