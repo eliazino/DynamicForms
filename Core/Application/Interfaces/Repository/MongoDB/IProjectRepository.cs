7025ed889b1f16a8a3f223cff877c75591b2e90a
eliazino <Elias.akin@hotmail.com>
2022-10-27 16:24:32 +0100

    interfaces for feature Auth service

--- Core/Application/Interfaces/Repository/MongoDB/IProjectRepository.cs
+++ Core/Application/Interfaces/Repository/MongoDB/IProjectRepository.cs
@@ -10,5 +10,7 @@ namespace Core.Application.Interfaces.Repository.MongoDB {
         Task<List<Project>> getProject();
         Task<List<Project>> getProject(string id);
         Task<bool> endProject(string id);
+        Task<bool> updateProject(Project project);
+        Task<List<Project>> getProject(List<string> projectID);
     }
 }
---
+++ Core/Application/Interfaces/Repository/MongoDB/IUserRepository.cs
@@ -0,0 +1,20 @@
+﻿using Core.Models.Entities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.Application.Interfaces.Repository.MongoDB {
+    public interface IUserRepository {
+        Task<List<Users>> getUser(string email);
+        Task<bool> createUser(Users user);
+        Task<bool> updateUser(Users user);
+    }
+
+    public interface IUserProjectRepository {
+        Task<List<ProjectOwners>> getUser(string email);
+        Task<bool> createUser(ProjectOwners user);
+        Task<bool> updateUser(ProjectOwners user);
+
+    }
+}
