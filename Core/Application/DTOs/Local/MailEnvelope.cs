dfd9557ed7f6e881b8af459d2ae728d2ee3b9a28
eliazino <Elias.akin@hotmail.com>
2022-10-27 16:23:43 +0100

    DTO

---
+++ Core/Application/DTOs/Local/MailEnvelope.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Core.Application.DTOs.Local {
+    public class MailEnvelope {
+        public string[] toName { get; set; }
+        public string[] toAddress { get; set; }
+        public string body { get; set; }
+        public string subject { get; set; }
+        public string[] attachment { get; set; }
+        public bool bodyIsPlainText { get; set; } = false;
+    }
+}
---
+++ Core/Application/DTOs/Local/User.cs
@@ -0,0 +1,9 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Core.Application.DTOs.Local {
+    public class UserDTO {
+        public string email { get; set; }
+    }
+}
