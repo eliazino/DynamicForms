d5adcbc585f625409e351e7ec5d627fbcd4dd618
eliazino <Elias.akin@hotmail.com>
2022-10-27 16:24:58 +0100

    usecase for auth feature

---
+++ Core/Application/Interfaces/UseCases/IUserUseCase.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using static Core.Application.DTOs.Response.ResponseFormat;
+
+namespace Core.Application.Interfaces.UseCases {
+    public interface IUserUseCase {
+        Task<RawResponse> login(string email, string code = null);
+        Task<RawResponse> inviteToProject(string email, string projectID, bool owner = false);
+        Task<RawResponse> removeFromProject(string email, string projectID);
+    }
+}
