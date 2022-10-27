3a4def04dab61708ca1f6cac7178e4ca9f7717d0
eliazino <Elias.akin@hotmail.com>
2022-10-27 16:24:47 +0100

    entities

--- Core/Models/Entities/Project.cs
+++ Core/Models/Entities/Project.cs
@@ -1,4 +1,5 @@
-﻿using Core.Application.DTOs.Request;
+﻿using Core.Application.DTOs.Local;
+using Core.Application.DTOs.Request;
 using Core.Exceptions;
 using Core.Shared;
 using System;
@@ -13,7 +14,8 @@ namespace Core.Models.Entities {
         public long dateCreated { get; protected set; }
         public int status { get; protected set; }
         public string clientID { get; protected set; }
-        public Project(ProjectDTO data) {
+        public HashSet<string> owners { get; protected set; }
+        public Project(ProjectDTO data, UserDTO user) {
             if (isNullOrEmpty(data.projectName, data.projectDetails))
                 throw new InputError("All fields are required");
             this.id = Cryptography.CharGenerator.genID(8, Cryptography.CharGenerator.characterSet.HEX_STRING);
@@ -21,6 +23,14 @@ namespace Core.Models.Entities {
             this.projectName = data.projectName;
             this.dateCreated = Utilities.getTodayDate().unixTimestamp;
             this.status = 1;
+            this.clientID = user.email;
+            this.owners = new HashSet<string> { user.email };
+        }
+        public void removeOwner(string email) {
+            this.owners.Add(email);
+        }
+        public void addOwner(string email) {
+            this.owners.Add(email);
         }
         public Project() { }
     }
